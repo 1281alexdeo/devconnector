381f49e7064e5c7623e1a8f2e52e0fbc305a0c2a
skyForge <1281alexdeo@gmail.com>
2018-05-19 22:36:31 +1200

    Post model done!!

--- models/Post.js
+++ models/Post.js
@@ -6,11 +6,56 @@ const PostSchema = new Schema({
   //connect each post to a USER
   user: {
     type: Schema.Types.ObjecId,
-    ref: 'users'
+    ref: 'users' //refering to the users collection
   },
   text: {
     type: String,
     required: true
+  },
+  //each post should have name and avatar
+  // so that we can preserve posts even after the account has been deleted
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    //should store arrays of user ids
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  //commets will be array of objects
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String
+      },
+      name: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  //date for the post
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
 
