f7a0c3c87a7a9a9e35af77f89970ed8888c23575
skyForge <1281alexdeo@gmail.com>
2018-05-19 19:14:32 +1200

    Continuing on Post model

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //CREATE Schema
-const PostSchema = new Schema({});
+const PostSchema = new Schema({
+  //connect each post to a USER
+  user: {
+    type: Schema.Types.ObjecId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
 
 //set it to a variable called User then = mongoose.model('name we wana use','name of the schema')
 module.exports = Post = mongoose.model('post', PostSchema);
