4f5af36d110e25402219d5c91e6758c2249df069
skyForge <1281alexdeo@gmail.com>
2018-05-21 17:58:18 +1200

    added front end form validation with bootstrap form validation styles

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
+
 class Register extends Component {
   constructor() {
     super();
@@ -33,6 +35,7 @@ class Register extends Component {
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="register">
         <div className="container">
@@ -46,22 +49,39 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      //classnames takes in string and object as args
+                      'is-invalid': errors.name //if errors.name exists
+                    })}
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
                     onChange={this.onInputChangeHandler.bind(this)}
                   />
+                  //conditionaly render a some JSX
+                  {errors.name ? (
+                    <div className="invalid-feedback">{errors.name} </div> //iferror.name exists then render this div
+                  ) : (
+                    //else
+                    <div> </div> //render empty div
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onInputChangeHandler.bind(this)}
                   />
+                  {errors.email ? (
+                    <div className="invalid-feedback">{errors.email} </div>
+                  ) : (
+                    <div> </div>
+                  )}
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
@@ -70,22 +90,36 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onInputChangeHandler.bind(this)}
                   />
+                  {errors.password ? (
+                    <div className="invalid-feedback">{errors.password} </div>
+                  ) : (
+                    <div> </div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onInputChangeHandler.bind(this)}
                   />
+                  {errors.password2 ? (
+                    <div className="invalid-feedback">{errors.password2} </div>
+                  ) : (
+                    <div> </div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
